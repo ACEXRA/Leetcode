613da1c0690533ee8915b75f337911f6a6afa56b
ACEXRA <rasheed.786ahamed@gmail.com>
2024-01-22 22:10:07 +0530

    Coding problems

---
+++ CodingProblems/src/Arrays/Sorting/BubbleSort.java
@@ -0,0 +1,25 @@
+package Arrays.Sorting;
+
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] arr={2,7,4,8,3,1,6,9,5};
+        bubbleSort(arr);
+    }
+
+    public static void bubbleSort(int[] arr) {
+        for(int i=0;i<arr.length;i++){
+            for(int j=i;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[i];
+                    arr[i]=arr[j];
+                    arr[j]=temp;
+                }
+            }
+        }
+        System.out.print("sorted array : ");
+        for(int n:arr){
+            System.out.print(n+" ");
+        }
+    }
+}
