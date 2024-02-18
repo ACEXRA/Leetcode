a2f3f0c2ad6748d0a43a810f8934a3d82d4979c0
ACEXRA <rasheed.786ahamed@gmail.com>
2024-02-18 22:00:03 +0530

    Array leetcode

--- CodingProblems/src/Arrays/SumOfLongestSubArray.java
+++ CodingProblems/src/Arrays/SumOfLongestSubArray.java
@@ -1,25 +1,27 @@
 package Arrays;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SumOfLongestSubArray {
     public static void main(String[] args) {
         int[] arr={10,5,2,7,1,9};
         sumOfLongestSubArray(arr,15);
     }
     public static void sumOfLongestSubArray(int[] arr,int target){
+        Map<Integer,Integer> mp=new HashMap<>();
+        int sum=0;
         int len=0;
         for(int i=0;i<arr.length;i++){
-            for(int j=i;j<arr.length;j++){
-                int sum=0;
-                int count=0;
-//                len=0;
-                for(int k=i;k<=j;k++){
-//                    System.out.print(arr[k]+" ");
-                    sum+=arr[k];
-                    count++;
-                }
-                if(sum==target){
-                    len=Math.max(count,len);
-                }
+            sum+=arr[i];
+            if(sum==target){
+                len=Math.max(len,i+1);
+            }
+            if(mp.containsKey(sum-target)){
+                len=Math.max(len,i-mp.get(sum-target));
+            }
+            if(!mp.containsKey(sum)){
+                mp.put(sum,i);
             }
         }
         System.out.println(len);
