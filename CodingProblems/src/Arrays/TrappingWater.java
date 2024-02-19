c4eb123235f9c7610447c3ab80cfb92087142a42
ACEXRA <rasheed.786ahamed@gmail.com>
2024-02-19 23:34:59 +0530

    Array leetcode

---
+++ CodingProblems/src/Arrays/TrappingWater.java
@@ -0,0 +1,33 @@
+package Arrays;
+
+public class TrappingWater {
+    public static void main(String[] args) {
+        int[] arr={3,0,0,2,0,4};
+        trappingWater(arr);
+    }
+    public static void trappingWater(int[] arr){
+        int start=0;
+        int end=arr.length-1;
+        int leftMax=0;
+        int rightMax=0;
+        int ans=0;
+        while(start<=end){
+            if(arr[start]<=arr[end]) {
+                if (arr[start] >= leftMax) {
+                    leftMax = arr[start];
+                } else {
+                    ans = ans + leftMax - arr[start];
+                }
+                start++;
+            }else{
+                if(arr[end]>=rightMax){
+                    rightMax=arr[end];
+                }else {
+                    ans=ans+rightMax-arr[end];
+                }
+                end--;
+            }
+        }
+        System.out.println(ans);
+    }
+}
