c7cab389e51fa28ca14d152e9abadac155202b18
ACEXRA <rasheed.786ahamed@gmail.com>
2024-02-29 21:43:18 +0530

    leetcode problems

---
+++ CodingProblems/src/Arrays2D/Pattern.java
@@ -0,0 +1,18 @@
+package Arrays2D;
+
+public class Pattern {
+    public static void main(String[] args) {
+        int[][] mat = {{1, 2, 3},
+            {4, 5, 6},
+            {7, 8, 9}};
+        pattern(mat);
+    }
+    public static void pattern(int[][] mat){
+        int i=0,j=0;
+        int n= mat.length;
+        boolean isUp=true;
+        for(int k=0;k<n*n;){
+
+        }
+    }
+}
