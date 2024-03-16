7482b2255aed86e4de03e9c295cddc2625e6111e
ACEXRA <rasheed.786ahamed@gmail.com>
2024-03-16 20:37:27 +0530

    leetcode problems

--- CodingProblems/src/BinarySearch/RightRotatedarray.java
+++ CodingProblems/src/BinarySearch/RightRotatedarray.java
@@ -2,15 +2,20 @@ package BinarySearch;
 
 public class RightRotatedarray {
     public static void main(String[] args) {
-        int[] arr={5,1,2,3,4};
-        System.out.println(rightRotatedArray(arr));
+        int[] arr={5,6,7,8,9,1,2,3,4};
+        System.out.println(rightRotatedArray(arr,0,arr.length-1));
     }
-    public static int rightRotatedArray(int[] arr){
-        for(int i=1;i<arr.length;i++){
-            if(arr[i]<arr[i-1]){
-                return i;
+    public static int rightRotatedArray(int[] arr,int s,int e){
+            int mid=(s+e)/2;
+            if(mid<e && arr[mid+1]<arr[mid]){
+                return mid+1;
             }
-        }
-        return 0;
+            if(mid>s && arr[mid]<arr[mid-1]){
+                return mid;
+            }
+            if(arr[e]>arr[mid]){
+                return rightRotatedArray(arr,s,mid-1);
+            }
+            return rightRotatedArray(arr,mid+1,e);
     }
 }
